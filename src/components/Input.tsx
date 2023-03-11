76e18f0788423dca14e63d3c78fd197a77175a99
hAppser <vip313z@ukr.net>
2023-03-11 14:39:55 +0200

    completed converter

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const Input:React.FC<Props> = (props) => {
     return(
-        <input type="number" placeholder="0" className="input px-2 bg-blue-600 placeholder:text-white text-white border-white border rounded-lg md:text-xl"
+        <input type="number" placeholder="Choose currency" className="input px-2 bg-blue-600 placeholder:text-white text-white border-white border rounded-lg md:text-xl"
           onChange={props.onChange}
           disabled={props.disabledInput}
           value={props.value}
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,5 @@
 import { useState, } from "react";
+import useDebounce from "../hooks/useDebounce";
 import { IRates } from "../types/IRates";
 import { ISelector } from "../types/ISelector";
 import Currency from "./Currency"
@@ -18,53 +19,55 @@ const Main = (props:any) => {
   
     const secondInputValue = !!moneyChanged ? moneyChanged[secondInput] : "";
     const firstInputValue = !!moneyChanged ? moneyChanged[mainInput] : "";
-  
+
     const switchHandle = (num:number, value:string) => {
       switch (value) {
         case "usd":
           return {
             eur: (num * props.usd) / props.eur,
-            uah: num * props.usd
+            uah: num * props.usd,
+            usd: num === 0 ? '' : num,
           };
         case "uah":
           return {
             eur: num / props.eur,
-            usd: num / props.usd
+            usd: num / props.usd,
+            uah: num === 0 ? '' : num,
           };
         case "eur":
           return {
             uah: num * props.eur,
-            usd: (num * props.usd) / props.eur
+            usd: (num * props.usd) / props.eur,
+            eur: num === 0 ? '' : num,
           };
         default:
-          return "";
+          return " ";
       }
     };
   
     const changer = (e:any, currency:string) => {
       const num = Number(e.target?.value);
   
-      const result:any = switchHandle(num, currency);
+      const result:any  = switchHandle(num, currency);
   
       setMoneyChanged(result);
     };
-    console.log('input',mainInput)
     return(
         
         <main className='flex flex-col text-center justify-around align-middle mt-10 bg-blue-600 rounded-3xl h-72'>
             <Currency 
-                inputOnChange={(e: Event) => changer(e, mainInput)}
+                inputOnChange={(e: React.ChangeEvent<HTMLInputElement>) => changer(e, mainInput)}
                 disabledInput={!mainInput}
-                newValue={firstInputValue}
-                firstCurrency={(e: any) => setMainInput(e.target?.value.toLowerCase())}
+                value={firstInputValue}
+                firstCurrency={(e: React.ChangeEvent<HTMLInputElement>) => setMainInput(e.target?.value.toLowerCase())}
                 selector={selector}
                 secondCurrency={secondInput.toUpperCase()} defaultValue={undefined}           
             /> 
             <Currency 
                 inputOnChange={(e: Event) => changer(e, secondInput)}
                 disabledInput={!secondInput}
-                newValue={secondInputValue}
-                firstCurrency={(e: any) => setSecondInput(e.target?.value.toLowerCase())}
+                value={secondInputValue}
+                firstCurrency={(e: React.ChangeEvent<HTMLInputElement>) => setSecondInput(e.target?.value.toLowerCase())}
                 selector={selector}
                 secondCurrency={mainInput.toUpperCase()} defaultValue={undefined}           
             /> 
